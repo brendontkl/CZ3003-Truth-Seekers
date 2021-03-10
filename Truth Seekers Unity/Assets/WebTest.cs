35a57b2a726039569f14b542d9059b9e30a2d131
brendontkl <61409315+brendontkl@users.noreply.github.com>
2021-03-11 04:38:31 +0800

    Add files via upload

---
+++ Truth Seekers Unity/Assets/WebTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        WWW request = new WWW("http://localhost/truthseekers/webtest.php");
+        yield return request;
+        Debug.Log(request.text);
+    }
+
+  
+}
