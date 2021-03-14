c149d9476dd2cc58c6df2446abebcd4e21ee23d5
brendontkl <btkl123@gmail.com>
2021-03-14 20:01:59 +0800

    Added Main menu changes
    Added main menu song, intro video and etc

---
+++ Assets/Scripts/Wait.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+    public float wait_time = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitIntro());
+    }
+
+    IEnumerator WaitIntro()
+    {
+        yield return new WaitForSeconds(wait_time);
+        SceneManager.LoadScene(1);
+
+    }
+
+}
